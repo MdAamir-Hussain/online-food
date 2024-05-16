8ec8038b58677f11a4dd843f4efcc2f41dd676ae
MdAamir-Hussain <mdaamirhussain319@gmail.com>
2024-05-16 17:04:45 +0530

    first commit

---
+++ src/components/layout/HomeMenu.js
@@ -0,0 +1,11 @@
+export default function HomeMenu()
+{
+    return(
+        <section className="">
+            <div className="text-center">
+            <h3 className="uppercase text-gray-500 font-semibold">Check out</h3>
+            <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+            </div>
+        </section>
+    );
+}
